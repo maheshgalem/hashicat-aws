a1e880f9b75e24da6a9e6929b294687efadf4a43
Mahesh Galem <maheshgalem@gmail.com>
2022-03-24 21:22:22 +0000

    Added s3 bucket module

--- main.tf
+++ main.tf
@@ -118,10 +118,6 @@ resource "aws_eip_association" "hashicat" {
   allocation_id = aws_eip.hashicat.id
 }
 
-resource "aws_s3_bucket" "hashicat" {
-  bucket_prefix = var.bucket_prefix
-}
-
 resource "aws_instance" "hashicat" {
   ami                         = data.aws_ami.ubuntu.id
   instance_type               = var.instance_type
--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,7 +2,7 @@ module "s3_bucket" {
     source = "terraform-aws-modules/s3-bucket/aws"
     version = "2.8.0"
 
-    bucket = "my-S3-bucket"
+    bucket = "maheshgalem-S3-bucket"
     acl    = "private"
 
     versioning = {
